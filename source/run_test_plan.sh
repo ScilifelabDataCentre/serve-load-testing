16c606681817568e21d0ddd204a466ed172e32e9
alfredeen <johan.alfredeen@gmail.com>
2024-01-18 16:35:10 +0100

    More settings and configuration to customize the user app behaviour

--- source/run_test_plan.sh
+++ source/run_test_plan.sh
@@ -2,10 +2,13 @@
 
 set -o errexit
 
+# Activate the virtual env
 source ./.venv/bin/activate
 
-# Start user app tests
-python3 ./tests-dev/appviewer_requestshtml.py
+# Set the env variables from this project
+set -o allexport; source .env; set +o allexport
 
-# Start locust tests
-locust --headless -f ./tests/test_verify_setup.py --users 1 --run-time 10s --html ./reports/locust-report-verify-setup.html
+# Run 2 sets of tests in parallel: the non-locust user apps and the locust test plan
+
+python3 ./tests-dev/appviewer_requestshtml.py & \
+locust --headless -f ./tests/test_plan_normal.py --users 10 --run-time 60s --html ./reports/locust-report-normal.html
--- source/tests-dev/appviewer_requestshtml.py
+++ source/tests-dev/appviewer_requestshtml.py
@@ -7,17 +7,27 @@ Note that shiny proxy pods are configured with
 - heartbeat-timeout=60s
 """
 
-from time import time
+from time import time, sleep
 import requests
 from requests_html import HTMLSession
 import warnings
 warnings.filterwarnings("ignore")
 
 
+# SETTINGS
 
+# The user app URL to open
 #URL = "https://loadtest-shinyproxy.serve-dev.scilifelab.se/app/loadtest-shinyproxy"
 URL = "https://loadtest-shinyproxy2.staging.serve-dev.scilifelab.se/app/loadtest-shinyproxy2"
 
+# The maximum number of apps to open
+MAX_APPS = 4
+
+# The initial delay to waut before opening any user apps
+INITIAL_DELAY_SECONDS = 10
+
+# The wait time between opening user apps
+OPEN_APPS_WAIT_TIME_SECONDS = 10
 
 
 
@@ -27,12 +37,14 @@ def apps_runner(n_requests: int = 1):
         :param n_requests int: The number of the app to be opened. Use with caution!
     """
 
-    max_apps = 2
-    if n_requests > max_apps:
-        raise Exception(f"Too many user apps requested to be opened. Max = {max_apps}")
+    if n_requests > MAX_APPS:
+        raise Exception(f"Too many user apps requested to be opened. Max = {MAX_APPS}")
 
     start_time = time()
     n_fails = 0
+
+    sleep(INITIAL_DELAY_SECONDS)
+    print("START apps_runner")
 
     for i in range(1,n_requests+1):
         print(f"Iteration: {i}")
@@ -43,6 +55,8 @@ def apps_runner(n_requests: int = 1):
         except Exception as ex:
             n_fails += 1
             print(f"Error while opening a user app {ex}")
+
+        sleep(OPEN_APPS_WAIT_TIME_SECONDS)
 
     duration_s = time() - start_time
     print(f"Duration (sec) for opening {n_requests} user apps = {duration_s}. Nr failures = {n_fails}")
@@ -72,5 +86,5 @@ def __test_open_sync(n_requests):
 
 if __name__ == "__main__":
     print("DEBUG: Begin running appviewer_requestshtml.py")
-    __test_open_sync(2)
+    __test_open_sync(4)
     print("DEBUG: Completed running appviewer_requestshtml.py")
